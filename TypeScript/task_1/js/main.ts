959e57c430cc55431c6a952b625135cb657466e2
samdaphbynet <lj8@hotmail.fr>
2024-04-16 13:35:47 +0200

    Writing a class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,6 +1,4 @@
-// 1. Let's build a Teacher interface
-
-
+// -------------------------1. Let's build a Teacher interface-----------------------------
 // Create a Teacher interface
 export interface Teacher {
     readonly firstName: string;
@@ -20,8 +18,10 @@ const teacher3: Teacher = {
     contract: false,
   };
   
-//   console.log(teacher3);
+// console.log(teacher3);
+//-------------------------------------------------------------------------------------------
 
+// -------------------------2. Extending the Teacher class-----------------------------------
 // Create a Directors that extends Teacher interface
 interface Directors extends Teacher {
     numberOfReports: number;
@@ -35,8 +35,12 @@ const director1: Directors = {
     fullTimeEmployee: true,
     numberOfReports: 17,
 };
-//   console.log(director1);
+// console.log(director1);
+//--------------------------------------------------------------------------------------------
 
+// -------------------------3. Printing teachers--------------------------------------------
+
+// function that return returns the first letter of the firstName and the full lastName
 function printTeacher(firstName: string, lastName: string): string {
     // return `${firstName[0]}. ${lastName}`; firstName = "Jon", lastName = "Don" ==>  Output : J. Don
     const firstLitter = firstName.charAt(0).toUpperCase();
@@ -52,4 +56,39 @@ interface printTeacherFunction {
 
 // Exemple usage
 const result: string = printTeacher("Jon", "Don")
-console.log(result);
+// console.log(result);
+//----------------------------------------------------------------------------------------
+
+// ---------------------------4. Writing a class--------------------------------------------
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string
+    lastName: string
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+// Exemple usage 
+const student = new StudentClass("Don", "Jone")
+console.log(student.workOnHomework())
+console.log(student.displayName())
+//----------------------------------------------------------------------------------------
