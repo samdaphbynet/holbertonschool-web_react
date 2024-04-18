deebffbcbf5722094f986a83cc099e525170731b
samdaphbynet <lj8@hotmail.fr>
2024-04-18 19:28:55 +0200

    write a subject class in the same namespace

---
+++ TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+// create class Subject inside namespace
+namespace Subjects {
+    export class Subject {
+        teacher: Teacher;
+
+        setTeacher(teacher: Teacher) {
+            this.teacher = teacher;
+        }
+    }
+}
