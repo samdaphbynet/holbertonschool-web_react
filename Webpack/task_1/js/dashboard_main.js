24e8c3241398a1bb87c7d4d517c26a7ada536b20
samdaphbynet <lj8@hotmail.fr>
2024-04-20 23:01:33 +0200

    DELETE function ready

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,18 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(document).ready(function() {
-    $("body").append("<p>Holberton Dashboard</p>");
-    $("body").append("<p>Dashboard data for the students</p>");
-    $("body").append("<button>Click here to get started</button>")
-    $("body").append("<p id='count'></p>")
-    $("body").append("<p>Copyright - Holberton School</p>")
 
-    let count = 0;
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>")
+$("body").append("<p id='count'></p>")
+$("body").append("<p>Copyright - Holberton School</p>")
 
-    const updateCounter = _.debounce(function() {
-        count++;
-        $("#count").text(`${count} clicks on the button`)
-    }, 500)
+let count = 0;
 
-    $("button").on('click', (updateCounter));
-});
+const updateCounter = _.debounce(function() {
+    count++;
+    $("#count").text(`${count} clicks on the button`)
+}, 500)
+
+$("button").on('click', (updateCounter));
