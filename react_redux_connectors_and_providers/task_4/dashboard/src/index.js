378fb84148021ca921bc8934a2469424cc01e0a4
samdaphbynet <lj8@hotmail.fr>
2024-06-13 10:28:03 +0200

    15. Combine store: modify the application

--- react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import {thunk} from "redux-thunk"
 import { Provider } from "react-redux";
 import App from "./App/App";
-import uiReducer, { initialState } from "./reducers/uiReducer";
-import { Map } from "immutable";
+import rootReducer, { initialState } from "./reducers/uiReducer";
 
 const composeDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  uiReducer,
-  Map(initialState),
+  combineReducers(rootReducer),
+  initialState,
   composeDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
